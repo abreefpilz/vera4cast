1b434b2bd1a0f83adda1106bce2eb073e3935d50
Austin Delany <addelany@gmail.com>
2025-08-08 09:17:21 -0500

    Update quality_bundles.R

--- submission_processing/quality_bundles.R
+++ submission_processing/quality_bundles.R
@@ -83,7 +83,7 @@ process_me <- function(path) {
 }
 
 
-model_paths <- model_paths[1:100]
+model_paths <- model_paths[101:200]
 
 # We use future_apply framework to show progress while being robust to OOM kils.
 # We are not actually running on multi-core, which would be RAM-inefficient
